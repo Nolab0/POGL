84ee41216898b80239e849159e6584c72eb3a445
Nolab <thibault.boutet@epita.fr>
2023-06-29 14:19:58 +0200

    start api call

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
+#include <curl/curl.h>
+
+#include "json.hpp"
 #include "camera.h"
 #include "init.h"
 #include "particles.h"
 
+using json = nlohmann::json;
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response)
+{
+    size_t totalSize = size * nmemb;
+    response->append((char*)contents, totalSize);
+    return totalSize;
+}
+
+std::string getWeatherData() {
+    CURL* curl;
+    CURLcode res;
+    std::string apiResponse;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        // Get the meteo info from Paris
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&hourly=temperature_2m,rain&forecast_days=1");
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &apiResponse);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+        {
+            std::cerr << "Failed to perform API request: " << curl_easy_strerror(res) << std::endl;
+        }
+        else
+        {
+            json data = json::parse(apiResponse);
+            std::cout << data << std::endl;
+            std::cout << data.value("hourly", "not found") << std::endl;
+        }
+
+        // Clean up
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
+    return apiResponse;
+}
+
 int main() {
+    std::string apiResponse = getWeatherData();
 
     GLFWwindow* window = init_glfw();
 
